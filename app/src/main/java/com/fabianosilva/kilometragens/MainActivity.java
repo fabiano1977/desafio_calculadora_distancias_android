bd0aedca5da5748746c1c944e3ade3c04375c98d
fabiano monteiro <fabiano.tecnologoADS@outlook.com>
2021-05-01 02:05:11 -0300

    Calculadora de distâncias

---
+++ app/src/main/java/com/fabianosilva/kilometragens/MainActivity.java
@@ -0,0 +1,36 @@
+package com.fabianosilva.kilometragens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editCentimetros;
+    private TextView resultadoMetros;
+    private TextView resultadoQuilometros;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editCentimetros = findViewById(R.id.editTextViewNumberDecimalCentimetros);
+        resultadoMetros = findViewById(R.id.textViewMetros);
+        resultadoQuilometros = findViewById(R.id.textViewQuilometros);
+
+    }
+
+    public void calcularMetragens (View view){
+        double centimetros = Double.parseDouble(editCentimetros.getText().toString());
+
+        double totalMetros = centimetros / 100;
+        double totalQuilometros = centimetros / 100000;
+
+        resultadoQuilometros.setText(totalQuilometros + " Km.");
+        resultadoMetros.setText(totalMetros + " m.");
+    }
+}
